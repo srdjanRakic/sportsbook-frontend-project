3bd59b43e07d27ebc35da2046ba85b96f4ff6082
Srdjan Rakic <srg_ian@hotmail.com>
2017-10-05 15:34:56 +0200

    Colorpicker installed.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import vSelect from 'vue-select';
+import { Chrome } from 'vue-color';
 import Vue from 'vue';
 import App from './App';
 import AlertCmp from './components/Shared/Alert';
 import { store } from './store';
 
+
 require('./assets/css/main.css');
 
+Vue.component('v-color-picker', Chrome);
 Vue.component('v-select', vSelect);
 Vue.component('app-alert', AlertCmp);
 Vue.config.productionTip = false;
