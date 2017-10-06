7fb820773bec9c2770c35e17f4d3826f6e21fc07
Srdjan Rakic <srg_ian@hotmail.com>
2017-10-06 10:40:59 +0200

    Added color picker modal.

---
+++ src/components/Shared/ColorPickerModal.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-if="showModal">
+    <transition name="modal">
+      <div class="modal-mask">
+        <div class="modal-wrapper">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" @click="showModal=false">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+                <h4 class="modal-title">Modal title</h4>
+              </div>
+              <div class="modal-body">
+                modal body
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['showModal'],
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,23 @@
+// Absolute imports
 import vSelect from 'vue-select';
-import { Chrome } from 'vue-color';
+import Chrome from 'vue-color';
 import Vue from 'vue';
+
+// Relative imports
 import App from './App';
 import AlertCmp from './components/Shared/Alert';
+import ColorPickerModal from './components/Shared/ColorPickerModal';
 import { store } from './store';
 
-
+// Global CSS
 require('./assets/css/main.css');
 
+// Global components
 Vue.component('v-color-picker', Chrome);
 Vue.component('v-select', vSelect);
 Vue.component('app-alert', AlertCmp);
+Vue.component('color-picker-modal', ColorPickerModal);
+
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
