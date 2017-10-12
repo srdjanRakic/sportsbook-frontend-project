9309c4138a392b63e861529aabe5322a403c2e16
Srdjan Rakic <srg_ian@hotmail.com>
2017-10-12 10:39:36 +0200

    BodyText save implemented.

--- src/components/Settings/GlobalFonts/BodyText/BodyText.vue
+++ src/components/Settings/GlobalFonts/BodyText/BodyText.vue
@@ -10,20 +10,29 @@
       <div class="list-group">
         <div class="list-group-item">
           <label>Font-family</label>
-          <v-select
-            :options="bodyText.fontFamily"
-            label="text">
-           </v-select>
-          <label>Font-family</label>
-          <v-select
-            :options="bodyText.fontWeight"
-            label="text">
-          </v-select>
+          <select class="form-control" v-model="selectedFontFamily">
+            <option
+            v-for="(option, index) in fontFamily" :key="index"
+            :value="option">
+            {{ option.value }}
+            </option>
+          </select>
+          <label>Font-weight</label>
+          <select class="form-control" v-model="selectedFontWeight">
+            <option
+            v-for="(option, index) in fontWeight" :key="index"
+            :value="option">
+            {{ option.value }}
+            </option>
+          </select>
           <label>Font-size</label>
-          <v-select
-           :options="bodyText.fontSize"
-           label="text">
-          </v-select>
+          <select class="form-control" v-model="selectedFontSize">
+            <option
+            v-for="(option, index) in fontSize" :key="index"
+            :value="option">
+            {{ option.value }}
+            </option>
+          </select>
         </div>
       </div>
     </div>
@@ -33,22 +42,38 @@
 <script>
 export default {
   name: 'body-text',
-  data: () => ({
-    selectedBodyText: {
-      fontFamily: '',
-      fontWeight: '',
-      fontSize: '',
-    },
-  }),
   computed: {
-    bodyText: {
-      // getter
+    fontFamily() {
+      return this.$store.getters.bodyText.fontFamily;
+    },
+    fontWeight() {
+      return this.$store.getters.bodyText.fontWeight;
+    },
+    fontSize() {
+      return this.$store.getters.bodyText.fontSize;
+    },
+    selectedFontFamily: {
       get() {
-        return this.$store.getters.bodyText;
+        return this.fontFamily.find(x => x.selected === true);
       },
-      // setter
-      set(newValue) {
-        this.$store.dispatch('updateBodyText', newValue);
+      set(val) {
+        this.$store.dispatch('updateBodyTextFontFamily', val);
+      },
+    },
+    selectedFontWeight: {
+      get() {
+        return this.fontWeight.find(x => x.selected === true);
+      },
+      set(val) {
+        this.$store.dispatch('updateBodyTextFontWeight', val);
+      },
+    },
+    selectedFontSize: {
+      get() {
+        return this.fontSize.find(x => x.selected === true);
+      },
+      set(val) {
+        this.$store.dispatch('updateBodyTextFontSize', val);
       },
     },
   },
--- src/store/generalSettings/globalFonts/bodyText/index.js
+++ src/store/generalSettings/globalFonts/bodyText/index.js
@@ -2,33 +2,84 @@ export default {
   state: {
     bodyText: {
       fontFamily: [
-        { text: 'State 1' },
-        { text: 'State 2' },
-        { text: 'State 3' },
-        { text: 'State 4' },
+        { value: 'State 1', selected: false },
+        { value: 'State 2', selected: true },
+        { value: 'State 3', selected: false },
+        { value: 'State 4', selected: false },
       ],
       fontWeight: [
-        { text: 'State 1' },
-        { text: 'State 2' },
-        { text: 'State 3' },
-        { text: 'State 4' },
+        { value: 'State 1', selected: true },
+        { value: 'State 2', selected: false },
+        { value: 'State 3', selected: false },
+        { value: 'State 4', selected: false },
       ],
       fontSize: [
-        { text: 'State 1' },
-        { text: 'State 2' },
-        { text: 'State 3' },
-        { text: 'State 4' },
+        { value: 'State 1', selected: false },
+        { value: 'State 2', selected: false },
+        { value: 'State 3', selected: false },
+        { value: 'State 4', selected: true },
       ],
     },
   },
   mutations: {
-    updateBodyText(state, payload) {
-      state.globalFonts = payload;
+    updateBodyTextFontFamily(state, payload) {
+      const fontFamilySettings = state.bodyText.fontFamily;
+
+      // unselect the previous selected option
+      const previouslySelectedOption = fontFamilySettings.find(x => x.selected === true);
+      previouslySelectedOption.selected = false;
+      // eslint-disable-next-line
+      console.log(previouslySelectedOption);
+
+      // set the new selected option
+      const newSelectedOption = fontFamilySettings.find(x => x === payload);
+      newSelectedOption.selected = true;
+
+      // eslint-disable-next-line
+      console.log(newSelectedOption);
+    },
+    updateBodyTextFontWeight(state, payload) {
+      const fontWeightSettings = state.bodyText.fontWeight;
+
+      // unselect the previous selected option
+      const previouslySelectedOption = fontWeightSettings.find(x => x.selected === true);
+      previouslySelectedOption.selected = false;
+      // eslint-disable-next-line
+      console.log(previouslySelectedOption);
+
+      // set the new selected option
+      const newSelectedOption = fontWeightSettings.find(x => x === payload);
+      newSelectedOption.selected = true;
+
+      // eslint-disable-next-line
+      console.log(newSelectedOption);
+    },
+    updateBodyTextFontSize(state, payload) {
+      const fontSizeSettings = state.bodyText.fontSize;
+
+      // unselect the previous selected option
+      const previouslySelectedOption = fontSizeSettings.find(x => x.selected === true);
+      previouslySelectedOption.selected = false;
+      // eslint-disable-next-line
+      console.log(previouslySelectedOption);
+
+      // set the new selected option
+      const newSelectedOption = fontSizeSettings.find(x => x === payload);
+      newSelectedOption.selected = true;
+
+      // eslint-disable-next-line
+      console.log(newSelectedOption);
     },
   },
   actions: {
-    updateBodyText({ commit }, payload) {
-      commit('updateBodyText', payload);
+    updateBodyTextFontFamily({ commit }, payload) {
+      commit('updateBodyTextFontFamily', payload);
+    },
+    updateBodyTextFontWeight({ commit }, payload) {
+      commit('updateBodyTextFontWeight', payload);
+    },
+    updateBodyTextFontSize({ commit }, payload) {
+      commit('updateBodyTextFontSize', payload);
     },
   },
   getters: {
