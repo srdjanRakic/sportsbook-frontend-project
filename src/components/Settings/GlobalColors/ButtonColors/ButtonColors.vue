5fecf3e9dcb62ac188bd980d3cf3d91ea482530f
Srdjan Rakic <srg_ian@hotmail.com>
2017-10-18 08:12:37 +0200

    ButtonColors colorpicker implemented.

--- src/components/Settings/GlobalColors/ButtonColors/ButtonColors.vue
+++ src/components/Settings/GlobalColors/ButtonColors/ButtonColors.vue
@@ -13,12 +13,13 @@
             <div class="container-fluid">
               <div class="col-md-3" v-for="(item, i) in buttonColors" :key="i">
                 <div style="clear: both;">{{item.title}}</div>
-                <a @click="showSelectColorModal">
+                <a @click="showSelectColorModal(item.title, item.chosenColor)">
                   <span :style="{background: item.chosenColor}" class="color-box"></span>
                 </a>
               </div>
             </div>
           </div>
+           <color-picker-modal :colorTitle="optionTitle" :color="chosenColor" @onColorChosen="onColorChosen"></color-picker-modal>
         </div>
       </div>
     </div>
@@ -28,14 +29,24 @@
 <script>
 export default {
   name: 'button-colors',
+  data: () => ({
+    optionTitle: '',
+    chosenColor: '',
+  }),
   computed: {
     buttonColors() {
       return this.$store.getters.buttonColors;
     },
   },
   methods: {
-    showSelectColorModal() {
+    showSelectColorModal(title, color) {
+      this.optionTitle = title;
+      this.chosenColor = color;
+
       this.$store.dispatch('toggleSelectColorModal', true);
+    },
+    onColorChosen(val) {
+      this.$store.dispatch('updateButtonColors', val);
     },
   },
 };
--- src/store/generalSettings/globalColors/buttonColors/index.js
+++ src/store/generalSettings/globalColors/buttonColors/index.js
@@ -8,8 +8,38 @@ export default {
     ],
   },
   mutations: {
+    updateButtonColors(state, payload) {
+      // eslint-disable-next-line
+      console.log(payload);
+
+      const tableColors = state.linkColors;
+       // eslint-disable-next-line
+      console.log(tableColors);
+
+      tableColors.forEach((item) => {
+        if (item.title === payload.title) {
+          // eslint-disable-next-line
+          console.log('Changing object');
+          // eslint-disable-next-line
+          console.log(item);
+
+          // eslint-disable-next-line
+          item.chosenColor = payload.chosenColor;
+        }
+      });
+
+      // eslint-disable-next-line
+      console.log(tableColors);
+    },
   },
   actions: {
+    updateButtonColors({ commit }, payload) {
+      // eslint-disable-next-line
+      console.log(`Payload: ${payload}`);
+      // eslint-disable-next-line
+      console.log(payload);
+      commit('updateButtonColors', payload);
+    },
   },
   getters: {
     buttonColors(state) {
@@ -17,4 +47,3 @@ export default {
     },
   },
 };
-
