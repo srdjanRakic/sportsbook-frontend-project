599c2db8abdc7009d5b669d8a042d92f81267662
Srdjan Rakic <srg_ian@hotmail.com>
2017-10-19 10:38:25 +0200

    Comments removed.

--- src/store/generalSettings/globalColors/buttonColors/index.js
+++ src/store/generalSettings/globalColors/buttonColors/index.js
@@ -9,35 +9,17 @@ export default {
   },
   mutations: {
     updateButtonColors(state, payload) {
-      // eslint-disable-next-line
-      console.log(payload);
-
-      const tableColors = state.linkColors;
-       // eslint-disable-next-line
-      console.log(tableColors);
+      const tableColors = state.buttonColors;
 
       tableColors.forEach((item) => {
         if (item.title === payload.title) {
-          // eslint-disable-next-line
-          console.log('Changing object');
-          // eslint-disable-next-line
-          console.log(item);
-
-          // eslint-disable-next-line
           item.chosenColor = payload.chosenColor;
         }
       });
-
-      // eslint-disable-next-line
-      console.log(tableColors);
     },
   },
   actions: {
     updateButtonColors({ commit }, payload) {
-      // eslint-disable-next-line
-      console.log(`Payload: ${payload}`);
-      // eslint-disable-next-line
-      console.log(payload);
       commit('updateButtonColors', payload);
     },
   },
--- src/store/generalSettings/globalColors/linkColors/index.js
+++ src/store/generalSettings/globalColors/linkColors/index.js
@@ -9,35 +9,17 @@ export default {
   },
   mutations: {
     updateLinkColors(state, payload) {
-      // eslint-disable-next-line
-      console.log(payload);
-
       const tableColors = state.linkColors;
-       // eslint-disable-next-line
-      console.log(tableColors);
 
       tableColors.forEach((item) => {
         if (item.title === payload.title) {
-          // eslint-disable-next-line
-          console.log('Changing object');
-          // eslint-disable-next-line
-          console.log(item);
-
-          // eslint-disable-next-line
           item.chosenColor = payload.chosenColor;
         }
       });
-
-      // eslint-disable-next-line
-      console.log(tableColors);
     },
   },
   actions: {
     updateLinkColors({ commit }, payload) {
-      // eslint-disable-next-line
-      console.log(`Payload: ${payload}`);
-      // eslint-disable-next-line
-      console.log(payload);
       commit('updateLinkColors', payload);
     },
   },
