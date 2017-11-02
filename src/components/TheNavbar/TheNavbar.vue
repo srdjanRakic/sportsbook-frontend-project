817727b5013ee457e4a48810d690d2f640af6944
srdjanRakic <srg_ian@hotmail.com>
2017-11-02 09:12:46 +0100

    Delete duplicate Navbar

--- src/components/TheNavbar/TheNavbar.vue
+++ src/components/TheNavbar/TheNavbar.vue
@@ -17,9 +17,9 @@
 		<!-- Collect the nav links, forms, and other content for toggling -->
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			<ul class="nav navbar-nav navbar-right">
-				<li><a href="#" target="_blank"><i class="glyphicon glyphicon-ok"></i> Save</a></li>
-        <li><a href="#" target="_blank"><i class="glyphicon glyphicon-circle-arrow-down"></i> Download HTML</a></li>
-        <li><a href="#" target="_blank"><i class="glyphicon glyphicon-remove"></i> Back to Admin</a></li>
+				<li><a href="#" target="_blank"><i class="glyphicon glyphicon-ok"></i>Save</a></li>
+        <li><a href="#" target="_blank"><i class="glyphicon glyphicon-circle-arrow-down"></i> Generate CSS</a></li>
+        <li><a href="#" target="_blank"><i class="glyphicon glyphicon-remove"></i>Back to Admin</a></li>
       </ul>
 			</div><!-- /.navbar-collapse -->
 		</div><!-- /.container-fluid -->
